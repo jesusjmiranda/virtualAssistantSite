baf69b471bd1031a9b799f71c2ffcf3960bbd25a
jjmiranda8 <jmlivingfree@gmail.com>
2021-08-06 18:46:43 -0400

    Add content for second ContentBanner. Add width and color props to Spacer

--- src/components/UI/Spacer/Spacer.js
+++ src/components/UI/Spacer/Spacer.js
@@ -1,7 +1,24 @@
 import styles from "./Spacer.module.css";
 
 const Spacer = (props) => {
-  return <div className={styles.Spacer}></div>;
+
+  let color = 'black';
+  let width = '90px';
+
+  if(props.color) {
+    color = props.color
+  }
+
+  if(props.width) {
+    width = props.width
+  }
+  
+
+  let style = {
+    backgroundColor: color,
+    width: width,
+  };
+  return <div className={styles.Spacer} style={style}></div>;
 };
 
 export default Spacer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,15 +10,27 @@ const Home = () => {
         <p className={styles.simpleText}>Virtual Business Support for the</p>
         <p className={styles.mediumText}>HEALTH & WELLNESS</p>
         <p className={styles.complexText}>entrepeneur</p>
-        <Spacer />
+
+        <Spacer color={'rgb(136, 76, 27)'} width={120} />
+
         <Button text={"WORK WITH ME"} />
       </ContentBanner>
       <ContentBanner color={"white"}>
-        <p>
-          I’m Nivia. I help heart-centered entrepreneurs in health and wellness
+        <p className={styles.firstParagraph}>
+          I’m Nivia. I help{" "}
+          <span className={styles.italicizedText}>
+            heart-centered entrepreneurs in health and wellness{" "}
+          </span>
           with the backend of their business so they can focus on what's most
           important:{" "}
         </p>
+
+        <p className={styles.secondParagraph}>
+          CLIENT SERVICING, IMPACTFUL CARE, AND SUSTAINED GROWTH
+        </p>
+
+        <Spacer color={'rgb(168,194,199)'} width={180} />
+
       </ContentBanner>
       <ContentBanner color={"rgb(211,140,126)"}></ContentBanner>
     </>
