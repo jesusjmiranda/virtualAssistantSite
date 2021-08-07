8f84afb988b1d744ad458fc95a1fa51034b1ba7e
jjmiranda8 <jmlivingfree@gmail.com>
2021-08-06 19:02:29 -0500

    Add the rest of the content and styling to the second banner and created ProvidedService component for the   the next ContentBanner

---
+++ src/components/ProvidedService/ProvidedService.js
@@ -0,0 +1,9 @@
+import styles from './ProvidedService.module.css'
+
+const ProvidedService = () => {
+    return (
+        <div className={styles.ProvidedService}></div>
+    )
+}
+
+export default ProvidedService
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,10 +11,11 @@ const Home = () => {
         <p className={styles.mediumText}>HEALTH & WELLNESS</p>
         <p className={styles.complexText}>entrepeneur</p>
 
-        <Spacer color={'rgb(136, 76, 27)'} width={120} />
+        <Spacer color={"rgb(136, 76, 27)"} width={80} />
 
         <Button text={"WORK WITH ME"} />
       </ContentBanner>
+
       <ContentBanner color={"white"}>
         <p className={styles.firstParagraph}>
           I’m Nivia. I help{" "}
@@ -29,10 +30,41 @@ const Home = () => {
           CLIENT SERVICING, IMPACTFUL CARE, AND SUSTAINED GROWTH
         </p>
 
-        <Spacer color={'rgb(168,194,199)'} width={180} />
+        <Spacer color={"rgb(168,194,199)"} width={200} />
 
+        <div className={styles.assumptions}>
+          <p className={styles.assumptionTitle}>LET ME GUESS...</p>
+          <p className={styles.assumption}>
+            ▽ Until now, you’ve been able to wear all the hats in your business;
+            lately you can’t shake the overwhelming feeling that you can’t keep
+            up.
+          </p>
+          <p className={styles.assumption}>
+            ▽ Your administrative tasks are sucking the joy out of your work,
+            and you want to get back to your passion — there just aren’t enough
+            hours in the day.
+          </p>
+          <p className={styles.assumption}>
+            ▽ You have the vision, game-changing ideas and goals to grow your
+            business, but technology isn’t your thing, and you’re starting to
+            feel STUCK.
+          </p>
+          <p className={styles.assumption}>
+            ▽ You want to be able to CONNECT with your base consistently on
+            Social Media but you’re not sure where to start...
+          </p>
+        </div>
+
+        <div className={styles.inspiration}>
+          YOU DON'T HAVE TO DO IT{" "}
+          <span className={styles.inspirationAccent}>alone</span>
+        </div>
+
+        <Spacer color={"rgb(168,194,199)"} width={160} />
       </ContentBanner>
-      <ContentBanner color={"rgb(211,140,126)"}></ContentBanner>
+      <ContentBanner color={"rgb(211,140,126)"}>
+        <div className={styles.thirdBannerTitle}>THIS IS HOW I CAN HELP...</div>
+      </ContentBanner>
     </>
   );
 };
