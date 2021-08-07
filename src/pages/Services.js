8293b567a275f36466c2ffccb22fa215741a409b
jjmiranda8 <jmlivingfree@gmail.com>
2021-08-07 10:15:08 -0500

    Add services first banner content

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,12 +1,17 @@
 import ContentBanner from "../components/UI/ContentBanner/ContentBanner";
+import styles from "./Services.module.css"
 
 const Services = () => {
   return (
-    <div>
-      <ContentBanner color={"rgb(211,140,126)"}></ContentBanner>
+    <>
+      <ContentBanner color={"rgb(211,140,126)"}>
+        <div className={styles.firstBannerText}>TAKE BACK YOUR TIME TO</div>
+        <div className={styles.firstBannerText}>FOCUS ON WHAT MATTERS</div>
+        <div className={styles.firstBannerTextTwo}>the most</div>
+      </ContentBanner>
       <ContentBanner color={"white"}></ContentBanner>
       <ContentBanner color={"rgb(211,140,126)"}></ContentBanner>
-    </div>
+    </>
   );
 };
 
